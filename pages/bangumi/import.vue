2ba91c57ec3756d585a2b4848cebf5c62d5a68f3
falstack <icesilt@outlook.com>
2019-11-10 09:01:23 +0800

    update

---
+++ pages/bangumi/import.vue
@@ -0,0 +1,24 @@
+<style lang="scss">
+</style>
+
+<template>
+  <div id="bangumi-import">
+    bangumi import
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BangumiImport',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
---
+++ pages/idol/import.vue
@@ -0,0 +1,24 @@
+<style lang="scss">
+</style>
+
+<template>
+  <div id="idol-import">
+    idol import
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IdolImport',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
